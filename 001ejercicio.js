b9986491d50977e3ccdf6c5f41e991c863bf9cb7
Maikoll Torres <maikolldtf@gmail.com>
2025-09-22 15:49:56 -0500

    first commit

---
+++ 001ejercicio.js
@@ -0,0 +1,58 @@
+/*console.log("hola primer programa JS")
+mi 
+primer
+comentario
+js
+*/
+/*console.log(nombre1)
+let nombre1 = "julian";
+console.log(nombre2)
+var nombre2 = "julian";
+console.log(nombre3)
+const nombre3 = "julian";
+*/
+
+/*console.log("calculdora, primer número 2, segundo numero 2")
+const num1 = 2+2;
+const num2 = 2-2;
+const num3 = 2*2;
+const num4 = 2/2;
+let varnull = varnull;
+console.log(typeof varnull);
+console.log(num1,num2,num3,num4)*/
+let varnull = null;
+let deco = "==========================="
+console.log(deco)
+console.log("          Factura")
+console.log(deco)
+const iva = 0.19;
+let empresa = "empresa piolin";
+let cliente = "Julian Monroy";
+let nit = 1012237516;
+console.log("Empresa:",empresa)
+console.log("cliente:",cliente)
+console.log("Nit:",nit)
+console.log(deco)
+let cantidad = 5;
+let precio = 2500;
+let producto = "leche";
+const subtotal = cantidad*precio;
+const total = subtotal+(subtotal*iva);
+console.log("Producto:",producto)
+console.log("Cantidad:",cantidad)
+console.log("Precio unitario:",precio)
+console.log("Subtotal sin iva:",subtotal)
+console.log("Total a pagar con iva",total)
+console.log(deco)
+console.log("tipos de variables:")
+console.log(typeof varnull);
+console.log(typeof deco)
+console.log(typeof empresa)
+console.log(typeof cliente)
+console.log(typeof nit)
+console.log(typeof cantidad)
+console.log(typeof precio)
+console.log(typeof producto)
+console.log(typeof subtotal)
+console.log(typeof total)
+console.log(deco)
---
+++ 002ejercicio.js
@@ -0,0 +1,7 @@
+let numero = 5;
+numero ++;
+console.log(numero)
+numero --;
+console.log(numero);
+numero+=3;
+console.log(numero);
